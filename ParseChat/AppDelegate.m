6ba0dd6366367dd4f9f6093387b02d0dd895cd1f
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-07-06 12:54:57 -0500

    Added basic login screen
    Login screen with secure password text field, sign up function that adds a new user to the Parse server, and logins. Handles missing information and sign up and login errors with alert messages. GIF with UI Changes: http://g.recordit.co/3UOzl2geSD.gif

--- ParseChat/AppDelegate.m
+++ ParseChat/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    // Add basic Parse configuration
+    ParseClientConfiguration *configuration = [ParseClientConfiguration configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+      configuration.applicationId = @"parseChatID";
+      configuration.server = @"https://blooming-thicket-89345.herokuapp.com/parse";
+    }];
+    [Parse initializeWithConfiguration:configuration];
+    
     return YES;
 }
 
